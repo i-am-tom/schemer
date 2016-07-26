76e025f24ddd9c5356011e6b55b851448f084ba8
Tom Harding <tomjharding@live.co.uk>
2016-07-26 15:44:40 +0100

    Tests for the Any Validator
    Adding in tests for the Any validator, which will match any value
    (and can optionally have more restrictions, allowing for totally
    customisable validators).

---
+++ test/Validator/Any.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Schema\Test\Validator;
+
+use Schemer\Validator\{Any,ValidatorAbstract};
+
+describe(Any::class, function () {
+    context('Unaltered', function () {
+        it('accepts strings', function () {
+            expect(
+                (new Any)
+                    ->validate('i-am-tom')
+                    ->errors()
+            )->toBe([]);
+        });
+
+        it('accepts floats', function () {
+            expect(
+                (new Any)
+                    ->validate(3.5)
+                    ->errors()
+            )->toBe([]);
+        });
+
+        it('accepts anything', function () {
+            expect(
+                (new Any)
+                    ->validate(null)
+                    ->errors()
+            )->toBe([]);
+        });
+    });
+
+    context('::but', function () {
+        it('allows passes for extra restrictions', function () {
+            expect(
+                (new Any)
+                    ->but(
+                        ValidatorAbstract::predicate(
+                            function ($x) { return $x === 1; },
+                            'not correct'
+                        )
+                    )
+                    ->validate(1)
+                    ->errors()
+            )->toBe([]);
+        });
+
+        it('rejects failures on extra restrictions', function () {
+            expect(
+                (new Any)
+                    ->but(
+                        ValidatorAbstract::predicate(
+                            function ($x) { return $x === 1; },
+                            'not correct'
+                        )
+                    )
+                    ->validate(2)
+                    ->errors()
+            )->toBe(['not correct']);
+        });
+    });
+});
