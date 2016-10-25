fa7d874a90249fd40565c86d43da93fde6cac0fa
Aaron J. Lang <aaronjameslang@googlemail.com>
2016-10-25 18:58:28 +0100

    Add tests

--- test/Formatter/Boolean.php
+++ test/Formatter/Boolean.php
@@ -9,6 +9,26 @@ describe(Boolean::class, function () {
         expect((new Boolean)->format(false))->toBe(false);
     });
 
+    it('handles the string "true"', function () {
+        expect((new Boolean)->format('true'))->toBe(true);
+    });
+
+    it('handles the string "false"', function () {
+        expect((new Boolean)->format('false'))->toBe(false);
+    });
+
+    it('handles the string "1"', function () {
+        expect((new Boolean)->format('1'))->toBe(true);
+    });
+
+    it('handles the string "0"', function () {
+        expect((new Boolean)->format('0'))->toBe(false);
+    });
+
+    it('handles the empty string', function () {
+        expect((new Boolean)->format(''))->toBe(false);
+    });
+
     it('casts non-boolean values', function () {
         expect((new Boolean)->format(112358))->toBe(true);
     });
